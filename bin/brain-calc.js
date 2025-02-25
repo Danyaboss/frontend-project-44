27f2bf1fd6181ea57c8bf59be4dc2f93335f4864
Your Name <you@example.com>
2025-02-25 15:08:16 +0300

    sixth

---
+++ bin/brain-calc.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import playGame from '../src/index.js';
+
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const getAnswer = () => {
+    const operators = ['+', '-', '*'];
+    const num1 = getRandom(1, 100);
+    const num2 = getRandom(1, 100);
+    const operator = operators[getRandom(0, operators.length)];
+
+    const calculate = (op, n1, n2) => {
+        if (op === '+') {
+            return n1 + n2;
+        }
+        if (op === '-') {
+             return n1 - n2;
+        }
+        if (op === '*') {return n1 * n2;
+        }
+    };
+
+    const answer = calculate(operator, num1, num2);
+    const expression = `${num1} ${operator} ${num2}`;
+
+    return { expression, answer: String(answer) }; 
+};
+
+const gameLogic = () => {
+    console.log('What is the result of the expression?');
+    const { expression, answer } = getAnswer();
+    return { question: expression, correctAnswer: answer };
+};
+
+playGame(gameLogic);
+
+
+    
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+const playGame = (gameLogic) => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+
+    let correctAnswersCount = 0;
+
+    while (correctAnswersCount < 3) {
+        const { question, correctAnswer } = gameLogic();
+        console.log(`Question: ${question}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+
+        if (userAnswer !== correctAnswer) {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+
+        console.log('Correct!');
+        correctAnswersCount += 1;
+    }
+
+    console.log(`Congratulations, ${name}!`);
+};
+
+export default playGame;
