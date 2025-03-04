76d785a0b928d8c838030a4eb27c401efbae33d7
Your Name <you@example.com>
2025-03-04 20:43:04 +0300

    fix6

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
-import greetUser  from '../src/cli.js'; 
+import readlineSync from 'readline-sync';
 
-greetUser();
+const greetUser  = () => {
+    const name = readlineSync.question('May I have your name? ');
+    return name; 
+};
+
+const startGame = () => {
+    console.log('Welcome to the Brain Games!'); 
+    const name = greetUser ();
+    console.log(`Hello, ${name}!`); 
+    
+};
+
+startGame();
