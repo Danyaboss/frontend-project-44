3c243357105fdd9ec54b093d2ce90d9f3423063a
Your Name <you@example.com>
2025-03-04 13:31:45 +0300

    eighth

---
+++ bin/brain-progression.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import playGame from '../src/index.js';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateProgression = (length, start, step, hiddenIndex) => {
+    const progression = [];
+    for (let i = 0; i < length; i += 1) {
+        progression.push(start + i * step);
+    }
+    progression[hiddenIndex] = '..';
+    return progression;
+};
+
+const QuestionAndResult = () => {
+    const length = getRandomInt(5, 10);
+    const start = getRandomInt(1, 20);
+    const step = getRandomInt(1, 10);
+    const hiddenIndex = getRandomInt(0, length - 1);
+
+    const progression = generateProgression(length, start, step, hiddenIndex);
+    const correctAnswer = String(start + hiddenIndex * step);
+    const question = progression.join(' ');
+
+    return { question, correctAnswer };
+};
+
+const gameLogic = 'What number is missing in the progression?'
+
+playGame(gameLogic, QuestionAndResult);
