58d64edcf0247e6d645b99ebbea46e74d5133427
Your Name <you@example.com>
2025-03-04 20:09:22 +0300

    fix5

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greetUser  = () => {
+    console.log('Welcome to the Brain game!');
     const name = readlineSync.question('May I have your name? ');
     return name; 
 };
